fd5b4870f9a1f35af6a475214be4c35f8e8d2075
Stefano <stefanop21@outlook.es>
2024-04-13 15:05:58 -0500

    new(src/issues/hooks/useLabels.tsx): Hook para imlementar useQuery

---
+++ src/issues/hooks/useLabels.tsx
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import { gitHubApi } from '../../api/gitHubApi';
+import { Label } from '../interfaces/label';
+import { sleep } from '../../helpers/sleep';
+
+const getLabels = async (): Promise<Label[]> => {
+  await sleep(2);
+  const { data } = await gitHubApi.get<Label[]>('/labels');
+  return data;
+};
+
+export const useLabels = () => {
+  const labelsQuery = useQuery({
+    queryKey: ['labels'],
+    queryFn: getLabels,
+  });
+
+  return labelsQuery;
+};
