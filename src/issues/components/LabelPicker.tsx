4f1bf7adb004becc553419e63b7129dafffedba2
Stefano <stefanop21@outlook.es>
2024-04-13 14:44:23 -0500

    Update(src/issues/components/LabelPicker.tsx): Implementación de axios

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import { gitHubApi } from '../../api/gitHubApi';
+import { Label } from '../../interfaces/label';
+
+const getLabels = async (): Promise<Label[]> => {
+  const { data } = await gitHubApi.get<Label[]>('/labels');
+  return data;
+};
 
 export const LabelPicker = () => {
+  const labelsQuery = useQuery({
+    queryKey: ['labels'],
+    queryFn: getLabels,
+  });
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+      >
+        Primary
+      </span>
     </div>
-  )
-}
+  );
+};
