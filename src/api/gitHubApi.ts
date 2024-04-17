32b41c8c1c1b090352e69080b702ee0a7e80c817
Stefano <stefanop21@outlook.es>
2024-04-17 17:39:28 -0500

    Revert "update(src/api/gitHubApi.ts): Autenticación en GitHub"
    This reverts commit cddfdc4ca09647276abcaf3b4746abd01ea12a78.

--- src/api/gitHubApi.ts
+++ src/api/gitHubApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const githubApi = axios.create({
+export const gitHubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
   headers: {
     Authorization: '',
--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-import { githubApi } from '../../api/githubApi';
+import { gitHubApi } from '../../api/githubApi';
 import { Label } from '../interfaces';
 import { sleep } from '../../helpers/sleep';
 
 const getLabels = async (): Promise<Label[]> => {
   await sleep(2);
-  const { data } = await githubApi.get<Label[]>('/labels');
+  const { data } = await gitHubApi.get<Label[]>('/labels');
   return data;
 };
 
