52067cf334409abb9ff280239b66b1697a1fd1c2
Stefano <stefanop21@outlook.es>
2024-04-16 22:58:12 -0500

    update(src/api/gitHubApi.ts): Variables de entorno

--- src/api/gitHubApi.ts
+++ src/api/gitHubApi.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
   headers: {
-    Authorization:
-      'Bearer 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',
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
   },
 });
