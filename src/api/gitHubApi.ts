cddfdc4ca09647276abcaf3b4746abd01ea12a78
Stefano <stefanop21@outlook.es>
2024-04-16 22:02:35 -0500

    update(src/api/gitHubApi.ts): Autenticación en GitHub

--- src/api/gitHubApi.ts
+++ src/api/gitHubApi.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-export const gitHubApi = axios.create({
+export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
-  headers: {},
+  headers: {
+    Authorization:
+      'Bearer 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',
+  },
 });
--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-import { gitHubApi } from '../../api/gitHubApi';
+import { githubApi } from '../../api/githubApi';
 import { Label } from '../interfaces/label';
 import { sleep } from '../../helpers/sleep';
 
 const getLabels = async (): Promise<Label[]> => {
   await sleep(2);
-  const { data } = await gitHubApi.get<Label[]>('/labels');
+  const { data } = await githubApi.get<Label[]>('/labels');
   return data;
 };
 
