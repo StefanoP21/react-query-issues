0265e97565303794dd5eff9e62c973d455b70ee7
Stefano <stefanop21@outlook.es>
2024-04-16 22:38:34 -0500

    update(src/issues/views/ListView.tsx): Mostrar issues

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { IssueList } from '../components/IssueList';
 import { LabelPicker } from '../components/LabelPicker';
+import { useIssues } from '../hooks';
 
 export const ListView = () => {
   const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
+  const { issuesQuery } = useIssues();
 
   const onLabelChanged = (labelName: string): void => {
     selectedLabels.includes(labelName)
